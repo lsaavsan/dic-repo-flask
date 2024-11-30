a008d124cfa8c711ed4b9289035b7716ef2e7857
lsaavsan <lsaavsan@gmail.com>
2024-11-29 22:24:49 -0500

    trabajando sqlite3 ver2

--- app.py
+++ app.py
@@ -1,64 +1,62 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
+
 """ Seleccionar el Intérprete de Python:
 Presiona Ctrl+Shift+P y escribe "Python: Select Interpreter", luego selecciona el entorno virtual (env) que acabas de crear. """
 
 
 def get_db_connection():
-    conn=sqlite3.connect("database.db")
-
-    #Genera un diccionario cuya clave son los nombre de la columna
-    conn.row_factory=sqlite3.Row
+    conn = sqlite3.connect("database.db")
+    conn.row_factory = sqlite3.Row
     return conn
 
 app = Flask(__name__)
 
-# primer endpoint
 @app.route("/", methods=["GET"])
 def root():
     return render_template('index.html')
 
-# segundo endpoint
-@app.route("/home", methods=["GET"])
-def home():
-    return render_template('home.html')
-
-# tercer endpoint
-@app.route("/about", methods=["GET"])
-def about():
-    return render_template('about.html')
-
-# cuarto endpoint
 @app.route("/base", methods=["GET"])
 def base():
     return render_template('base.html')
 
-@app.route("/posts",methods=["GET"])
+@app.route("/home", methods=["GET"])
+def home():
+    return render_template('home.html')
+
+@app.route("/about", methods=["GET"])
+def about():
+    return render_template('about.html')
+
+@app.route("/post", methods=["GET"])
 def get_all_posts():
-    conn=get_db_connection()  
-    # modelo
-    posts=conn.execute('SELECT * FROM posts').fetchall()
-    #print(posts)
-    #print(conn)
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM posts').fetchall()
     conn.close()
-    #view
-    return render_template("post/posts.html",posts=posts)
+    return render_template('post/posts.html', posts=posts)
 
-
-
-
-@app.route("/post/<int:post_id>",methods=["GET"])
-def get_one_posts(post_id:int):
-    conn=get_db_connection()    
-    # modelo
-    post=conn.execute('SELECT * FROM posts WHERE id = ?',(post_id,)).fetchone() 
-    #print(posts)
-    #print(conn)
+@app.route("/post/<int:post_id>", methods=["GET"])
+def get_one_post(post_id: int):
+    conn = get_db_connection()
+    post = conn.execute('SELECT * FROM posts WHERE id = ?', (post_id,)).fetchone()
     conn.close()
-    #view
-    return render_template("post/post.html",post=post)
+    return render_template('post/post.html', post=post)
 
 
+@app.route("/post/create", methods=["GET", "POST"])
+def create_one_post():
+    if request.method == "POST":
+        title = request.form["title"]
+        content = request.form["content"]
+        conn = get_db_connection()
+        conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)', (title, content))
+        conn.commit()
+        conn.close()
+        return redirect(url_for('get_all_posts'))
+    elif request.method == "GET":
+        return render_template('post/create.html')
 
+
+# Iniciar el servidor
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
