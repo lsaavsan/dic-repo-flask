1108c46f659af455c0124db89efcef313a7375fa
lsaavsan <lsaavsan@gmail.com>
2024-11-29 21:19:20 -0500

    trabajando sqlite3

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from flask import Flask, render_template
-
+import sqlite3
 """ Seleccionar el Intérprete de Python:
 Presiona Ctrl+Shift+P y escribe "Python: Select Interpreter", luego selecciona el entorno virtual (env) que acabas de crear. """
+
+
+def get_db_connection():
+    conn=sqlite3.connect("database.db")
+
+    #Genera un diccionario cuya clave son los nombre de la columna
+    conn.row_factory=sqlite3.Row
+    return conn
 
 app = Flask(__name__)
 
@@ -9,7 +17,6 @@ app = Flask(__name__)
 @app.route("/", methods=["GET"])
 def root():
     return render_template('index.html')
-
 
 # segundo endpoint
 @app.route("/home", methods=["GET"])
@@ -25,6 +32,33 @@ def about():
 @app.route("/base", methods=["GET"])
 def base():
     return render_template('base.html')
+
+@app.route("/posts",methods=["GET"])
+def get_all_posts():
+    conn=get_db_connection()  
+    # modelo
+    posts=conn.execute('SELECT * FROM posts').fetchall()
+    #print(posts)
+    #print(conn)
+    conn.close()
+    #view
+    return render_template("post/posts.html",posts=posts)
+
+
+
+
+@app.route("/post/<int:post_id>",methods=["GET"])
+def get_one_posts(post_id:int):
+    conn=get_db_connection()    
+    # modelo
+    post=conn.execute('SELECT * FROM posts WHERE id = ?',(post_id,)).fetchone() 
+    #print(posts)
+    #print(conn)
+    conn.close()
+    #view
+    return render_template("post/post.html",post=post)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
