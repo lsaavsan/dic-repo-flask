a78f510cfac26b9d20b4b704b4ac21dcd5a3e620
lsaavsan <lsaavsan@gmail.com>
2024-12-02 21:26:39 -0500

    nuevo

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for,abort
 import sqlite3
 
 """ Seleccionar el Intérprete de Python:
@@ -20,6 +20,7 @@ def root():
 def base():
     return render_template('base.html')
 
+# Petición-get
 @app.route("/home", methods=["GET"])
 def home():
     return render_template('home.html')
@@ -28,6 +29,7 @@ def home():
 def about():
     return render_template('about.html')
 
+# Obtener todos los post
 @app.route("/post", methods=["GET"])
 def get_all_posts():
     conn = get_db_connection()
@@ -35,6 +37,8 @@ def get_all_posts():
     conn.close()
     return render_template('post/posts.html', posts=posts)
 
+
+# solo para uno
 @app.route("/post/<int:post_id>", methods=["GET"])
 def get_one_post(post_id: int):
     conn = get_db_connection()
@@ -55,6 +59,42 @@ def create_one_post():
         return redirect(url_for('get_all_posts'))
     elif request.method == "GET":
         return render_template('post/create.html')
+
+
+@app.route("/post/edit/<int:post_id>", methods=["GET", "POST"])
+def edit_one_post(post_id):
+    conn = get_db_connection()
+    post = conn.execute('SELECT * FROM posts WHERE id = ?', (post_id,)).fetchone()
+    conn.close()
+    if post is None:
+        abort(404)
+ 
+    if request.method == "POST":
+        title = request.form["title"]
+        content = request.form["content"]
+        conn = get_db_connection()
+        conn.execute('UPDATE posts SET title = ?, content = ? WHERE id = ?', (title, content, post_id))
+        conn.commit()
+        conn.close()
+        return redirect(url_for('get_all_posts'))
+    if request.method == "GET":
+        return render_template('post/edit.html', post=post)
+
+@app.route("/post/delete/<int:post_id>", methods=["POST"])
+def delete_one_post(post_id):
+    conn = get_db_connection()
+    post = conn.execute('SELECT * FROM posts WHERE id = ?', (post_id,)).fetchone()
+   
+    if post is None:
+        abort(404)
+ 
+    conn.execute('DELETE FROM posts WHERE id = ?', (post_id,))
+    conn.commit()
+    conn.close()
+    return redirect(url_for('get_all_posts'))
+
+
+
 
 
 # Iniciar el servidor
